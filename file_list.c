faf3ce6d12da0dbc83bd8721dbff2e4afeda0c2b
Stan Hatko <stan_hatko@live.com>
2025-04-19 15:23:47 -0400

    implement file_list_add

--- file_list.c
+++ file_list.c
@@ -10,21 +10,59 @@
 static struct open_file_entry list_open_files[MAX_OPEN_FILES];
 static pthread_mutex_t open_file_lock = PTHREAD_MUTEX_INITIALIZER;
 
-int check_if_open_nolock(const char *mc_filename)
+// Check if file is in list of open files.
+// Internal function that doesn't set lock.
+int file_list_check_nolock(const char *mc_filename)
 {
     for (int i = 0; i < MAX_OPEN_FILES; i++)
     {
-        struct open_file_entry s = list_open_files[i];
-        if (s.is_open && (strcmp(s.mc_filename, mc_filename) == 0))
+        struct open_file_entry *s = &(list_open_files[i]);
+        if (s->is_active && (strcmp(s->mc_filename, mc_filename) == 0))
             return i;
     }
     return -1;
 }
 
-int check_if_open(const char *mc_filename)
+// Check if file is in list of open files.
+// Return index if yes, -1 if no.
+int file_list_check(const char *mc_filename)
 {
     pthread_mutex_lock(&open_file_lock);
-    int r = check_if_open_nolock(mc_filename);
+    int r = file_list_check_nolock(mc_filename);
+    pthread_mutex_unlock(&open_file_lock);
+    return r;
+}
+
+// Check if file is in list of open files, if not, add it.
+// Return index if successful, -1 if failed.
+int file_list_add(const char *mc_filename)
+{
+    pthread_mutex_lock(&open_file_lock);
+
+    // If already in list, return that.
+    int r = file_list_check_nolock(mc_filename);
+    if (r >= 0)
+    {
+        pthread_mutex_unlock(&open_file_lock);
+        return r;
+    }
+
+    // Need to add to list.
+    for (int i = 0; i < MAX_OPEN_FILES; i++)
+    {
+        // Find available entry.
+        struct open_file_entry *s = &(list_open_files[i]);
+        if (s->is_active)
+            continue;
+
+        // Found entry that's available.
+        // Earlier validated that mc_filename is not too long.
+        strcpy(s->mc_filename, mc_filename);
+        s->is_active = true;
+        s->is_init = false;
+    }
+
+    // Failed since no available entries.
     pthread_mutex_unlock(&open_file_lock);
     return r;
 }
--- file_list.h
+++ file_list.h
@@ -10,17 +10,20 @@
 #include <stdlib.h>
 #include <string.h>
 
-#define MAX_OPEN_FILES 1024
+#define MAX_OPEN_FILES 256
+#define MC_PATH_BUF_LEN 296
+#define TEMP_PATH_BUF_LEN 72
 
 struct open_file_entry
 {
-    char *mc_filename;
-    char *local_filename;
+    char mc_filename[MC_PATH_BUF_LEN];
+    char local_filename[TEMP_PATH_BUF_LEN];
     int handles_count;
     pthread_mutex_t is_locked;
-    bool is_open;
+    bool is_active;
+    bool is_init;
     bool need_copy_end;
 };
 
-int check_if_open(const char *mc_filename);
-int open_file(const char *mc_filename);
+int file_list_check(const char *mc_filename);
+int file_list_add(const char *mc_filename);
