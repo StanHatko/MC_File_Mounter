f2fd581ed0230e82495fe1cd97c66edcfa086a6e
Stan Hatko <stan_hatko@live.com>
2025-04-20 07:56:45 -0400

    start using C++ internally, soon split into two programs, main C server and C++ handler program for requests

--- mc.cpp
+++ mc.cpp
@@ -5,12 +5,19 @@
  * License: GNU GPL
  */
 
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <iostream>
+#include <string>
 
+extern "C"
+{
 #include "common.h"
 #include "mc.h"
+}
+
+std::string remove_mount_prefix(std::string minio_path);
 
 char *mc_binary;
 int mc_mount_prefix_len;
@@ -26,7 +33,7 @@ void init_minio_mc_config()
     mc_mount_prefix = getenv("mc_mount_prefix");
     print_config_var("mc_mount_prefix", mc_mount_prefix);
     validate_config_len("mc_mount_prefix", mc_mount_prefix, MAX_LEN_COMPONENTS);
-    mc_mount_prefix_len = strlen(mc_mount_prefix);
+    mc_mount_prefix_len = std::strlen(mc_mount_prefix);
 
     mc_request_prefix = getenv("mc_request_prefix");
     print_config_var("mc_request_prefix", mc_request_prefix);
@@ -34,8 +41,14 @@ void init_minio_mc_config()
 }
 
 // Remove mount prefix in front of path to MinIO file.
-const char *remove_mount_prefix(const char *minio_path)
+std::string remove_mount_prefix(const char *minio_path)
 {
+    std::string r = minio_path;
+    if (r.length() < mc_mount_prefix_len)
+    {
+        throw std::string("minio_path not in mount directory");
+    }
+    r.replace(0, mc_mount_prefix_len, "");
     int nc = strlen(minio_path);
     if (nc < mc_mount_prefix_len)
         return NULL;
@@ -48,32 +61,24 @@ const char *remove_mount_prefix(const char *minio_path)
 
 int copy_to_minio(const char *local, const char *remote)
 {
-    char cmd[MINIO_CMD_BUF_LEN];
-
-    const char *r = remove_mount_prefix(remote);
-    if (r == NULL)
+    try
     {
-        fprintf(stderr, "Invalid destination path.");
-        return -1;
-    }
+        std::string cmd = mc_binary;
+        std::string r = remove_mount_prefix(remote);
 
-    // TODO: REPLACE BY posix_spawn which avoids shell escaping
-    sprintf(cmd, "mc cp \"%s\" \"%s/%s\"", local, mc_request_prefix, r);
-    return system(cmd);
+        // TODO: REPLACE BY posix_spawn which avoids shell escaping
+        sprintf(cmd, "mc cp \"%s\" \"%s/%s\"", local, mc_request_prefix, r);
+        return system(cmd);
+    }
+    catch (const std::string &e)
+    {
+        std::cerr << "Exception occurred:" << e << "\n";
+        return -1; // TODO adjust
+    }
 }
 
 int copy_from_minio(const char *remote, const char *local)
 {
-    char cmd[MINIO_CMD_BUF_LEN];
-
-    const char *r = remove_mount_prefix(remote);
-    if (r == NULL)
-    {
-        fprintf(stderr, "Invalid destination path.");
-        return -1;
-    }
-
-    // TODO: REPLACE BY posix_spawn which avoids shell escaping
-    sprintf(cmd, "mc cp \"%s/%s\" \"%s\"", mc_request_prefix, remote, local);
-    return system(cmd);
+    // TODO implement
+    return -1;
 }
--- mc.h
+++ mc.h
@@ -8,6 +8,5 @@
 #include "config.h"
 
 void init_minio_mc_config();
-const char *remove_mount_prefix(const char *minio_path);
 int copy_to_minio(const char *local, const char *remote);
 int copy_from_minio(const char *local, const char *remote);
