f282900fd3ac61ec54fd34ded17722154913a255
Stan Hatko <stan_hatko@live.com>
2025-04-27 21:51:16 -0400

    progress

--- bridge.py
+++ bridge.py
@@ -2,3 +2,9 @@
 Bridge between server process and FSSpec objects.
 Converts between socket representation and Python format.
 """
+
+
+def send_metadata(rfile):
+    """
+    Send metadata object in format server expects.
+    """
--- files_server.py
+++ files_server.py
@@ -14,10 +14,29 @@ import tempfile
 import fsspec
 
 
-def send_metadata():
+def get_path(self) -> str:
+    """
+    Get null-terminated UTF-8 path from input.
+    """
+    r = []
+    while True:
+        ch = self.rfile.read(1)
+        if ch == 0:
+            return b"".join(r).decode("UTF-8")
+        r.append(ch)
+
+
+def handler_process(manager, rfile: io.BufferedIOBase, timeout: float):
     """
     Send metadata object in format server expects.
     """
+
+    # Get type of request and on which path.
+    action = rfile.read(1)
+    path = get_path()
+    print(f"Perform operation {action} on {path}.")
+
+    # TODO
 
 
 class FileSocketServer(socketserver.StreamRequestHandler):
@@ -25,28 +44,8 @@ class FileSocketServer(socketserver.StreamRequestHandler):
     Class that responds to requests from FUSE connector and sends to Python processes.
     """
 
-    objects_db = {}
-
-    def get_path(self) -> str:
-        """
-        Get null-terminated UTF-8 path from input.
-        """
-        r = []
-        while True:
-            ch = self.rfile.read(1)
-            if ch == 0:
-                return b"".join(r).decode("UTF-8")
-            r.append(ch)
-
-    def get_cached_metadata(self, path: str) -> bool:
-        if path in objects_db:
-            return
-
-    def send_object_process(action, action: bytes, path: str) -> bool:
-        TODO
-
-    def start_object_process(action, action: bytes, path: str):
-        TODO
+    manager = None
+    objects_db = None
 
     def handle(self):
         """
@@ -55,31 +54,31 @@ class FileSocketServer(socketserver.StreamRequestHandler):
         Call out to other objects as fast as possible.
         """
 
-        # Get type of request and on which path.
-        action = self.rfile.read(1)
-        path = self.get_path()
-
-        # For get metadata, check cache first.
-        if action == b"G" and self.get_cached_metadata(path):
-            return
-
-        # If process already exists, send there.
-        if self.send_object_process(action, path):
-            return
-
-        # Need to start new process to handle this request.
-        self.start_object_process(action, path)
+        # Transfer control to subprocess.
+        mp.Process(
+            target=handler_process,
+            args=(
+                self.objects_db,
+                self.rfile,
+                self.timeout,
+            ),
+        )
 
 
 def main():
     """
     Function invoked when this program is run from command line.
     """
-
-    with socketserver.UnixStreamServer(
-        "/tmp/fs_server.socket",  # TODO: make configurable
-        FileSocketServer,
-    ) as server:
+    mp.set_start_method("fork", force=True)
+    with (
+        socketserver.UnixStreamServer(
+            "/tmp/fs_server.socket",  # TODO: make configurable
+            FileSocketServer,
+        ) as server,
+        mp.Manager() as manager,
+    ):
+        server.manager = manager
+        server.objects_db = manager.dict()
         server.serve_forever()
 
 
